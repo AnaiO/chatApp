2dd682a903ea6f4ee5c40c172a0b635ae7c5c461
AnaiO <anais.berton.io@gmail.com>
2023-02-27 17:34:27 +0100

    initialize mongoose connexion

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const app = express();
-require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
