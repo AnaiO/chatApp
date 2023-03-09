7fbb293583063410e2f4b28b428c8cd7f33cb269
AnaiO <anais.berton.io@gmail.com>
2023-03-09 15:18:54 +0100

    refacto header

--- public/src/App.js
+++ public/src/App.js
@@ -1,25 +1,14 @@
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignIn from './pages/SignIn';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Layout/Header';
 
 
 export default function App() {
   return (
     <BrowserRouter>
-      <header className="border-2 border-white my-8">
-        <ul className="flex justify-around text-white rounded-lg">
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/login'>Login</Link>
-          </li>
-          <li>
-            <Link to='signin'>SignIn</Link>
-          </li>
-        </ul>
-      </header>
+      <Header />
       <main>
         <Routes>
           <Route path='/login' element={<Login />} />
