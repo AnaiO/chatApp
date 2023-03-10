bb786aa2ebbde7e5dc23de72204423e7f88d3698
AnaiO <anais.berton.io@gmail.com>
2023-03-10 10:40:06 +0100

    clean passport file

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,7 +1,5 @@
 const LocalStrategy = require('passport-local');
-// const bcrypt = require('bcrypt');
 const User = require('../models/user');
-// const passport = require('passport');
 
 module.exports = function(passport) {
   passport.use('local-login', new LocalStrategy(function verify(username, password, done) {
@@ -19,19 +17,7 @@ module.exports = function(passport) {
       }
   
       return done(null, user);
-      // bcrypt.compare(password, user.password, (err, result) => {
-      //   if (err) {
-      //     return done(err);
-      //   }
-  
-      //   if (result == false) {
-      //     return done(null, false, { message: 'Incorrect password' });
-      //   }
-  
-      //   return done(null, user);
-      // });
-      
-    })
+    });
   }));
   
   passport.use('local-signup', new LocalStrategy(function verify(username, password, done) {
@@ -51,23 +37,8 @@ module.exports = function(passport) {
       newUser.save(function(err) {
         if (err) throw err;
         return done(null, newUser);
-      })
-  // 
-      // user.generatePassword(password)
-      
-      // bcrypt.compare(password, user.password, (err, result) => {
-      //   if (err) {
-      //     return done(err);
-      //   }
-  
-      //   if (result == false) {
-      //     return done(null, false, { message: 'Incorrect password' });
-      //   }
-  
-      //   return done(null, user);
-      // });
-      
-    })
+      });
+    });
   }));
 }
 
