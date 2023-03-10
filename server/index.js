fb45016342ad5bec86897012f7692e4d26f96750
AnaiO <anais.berton.io@gmail.com>
2023-03-10 11:40:50 +0100

    add session and connect with passport

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const passport = require('passport');
+const session = require('express-session');
 const mongooseConnection = require('./config/mangoose-connection');
 require('dotenv').config();
 const authRouter = require('./routes/auth');
@@ -7,9 +9,18 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: true }
+}));
+app.use(passport.session());
 
+//DB
 mongooseConnection();
 
+//Routes
 app.use('/', authRouter);
 
 const server = app.listen((process.env.PORT || 5001), () => {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const passport = require('passport');
 require('../config/passport')(passport);
 
-router.get('/', function(req, res) {
-  res.send('coucou')
-});
-router.post('/signup', passport.authenticate('local-signup'))
-router.post('/login', passport.authenticate('local-login'));
+
+router.post('/signup', passport.authenticate('local-signup', {
+  successRedirect: '/'
+}));
+
+router.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/'
+}));
+
 
 module.exports = router;
